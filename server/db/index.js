1f7a7f18212a02b425b7c30740439bdb81c4ec30
Andrew Poon <akpoon@gmail.com>
2019-07-06 16:52:42 -0700

    initial generic node express index setup

--- server/db/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoUri = 'mongodb://localhost/restaurant-menus';
-
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
-
-module.exports = db;
