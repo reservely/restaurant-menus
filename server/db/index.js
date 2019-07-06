bab8305112fe16545d7caf23092b049a4685e219
Andrew Poon <akpoon@gmail.com>
2019-07-06 02:57:20 -0700

    fixed eslint formatting

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+
 const mongoUri = 'mongodb://localhost/restaurant-menus';
 
-const db = mongoose.connect(mongoUri, {useNewUrlParser: true});
+const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 
 module.exports = db;
