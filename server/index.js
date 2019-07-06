f0fdc842ed9b1285c43c09cedc4d5406a1499836
Andrew Poon <akpoon@gmail.com>
2019-07-06 16:54:31 -0700

    initial generic node express index setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-const seed = require('./db/seed.js');
+// const seed = require('../db/seed.js');
 
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
