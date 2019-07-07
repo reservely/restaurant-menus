174ebc3631a538245985e41acae086e51a3569ac
Andrew Poon <akpoon@gmail.com>
2019-07-06 17:12:19 -0700

    some formatting corrections

--- db/seed.js
+++ db/seed.js
@@ -3,22 +3,22 @@ const db = require('./index.js');
 const Restaurant = require('./Restaurant.js');
 
 
-for (let i = 0; i < 100; i += 1) {
+for (let i = 0; i <= 100; i += 1) {
   const menusArray = [];
-  const menuCount = faker.random.number({min: 0, max: 6})
+  const menuCount = faker.random.number({ min: 0, max: 6 });
   for (let j = 0; j < menuCount; j += 1) {
     const sectionsArray = [];
-    const sectionCount = faker.random.number({min: 1, max: 6})
+    const sectionCount = faker.random.number({ min: 1, max: 6 });
     for (let k = 0; k < sectionCount; k += 1) {
       const itemsArray = [];
-      const itemCount = faker.random.number({min: 1, max: 6})
+      const itemCount = faker.random.number({ min: 1, max: 6 });
       for (let l = 0; l < itemCount; l += 1) {
         itemsArray.push({
           item_name: faker.commerce.productName(),
           item_description: faker.lorem.sentence(3),
           item_price: faker.commerce.price(1, 100.00, 2, '$'),
           item_option: faker.lorem.words(1)
-        })
+        });
       }
       sectionsArray.push(
         {
@@ -45,7 +45,7 @@ for (let i = 0; i < 100; i += 1) {
       console.log(err);
     }
   });
-};
+}
 
 
 console.log('Menus for 100 restaurants have been generated.');
