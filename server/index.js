43c5227581374083fea45b7b2012b1044ca0ff83
Andrew Poon <akpoon@gmail.com>
2019-07-08 14:20:48 -0700

    modified server side to adjust for a varying api request based on restaurant_id

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const Restaurant = require('../db/Restaurant.js');
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+
+app.get('/:restaurant_id/menus', (req, res) => (
+  Restaurant.find(req.params, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(data);
+  })
+));
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
