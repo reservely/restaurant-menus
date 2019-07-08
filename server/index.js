768583e1cdd3b26a781b8d73ccd3146324d318af
Andrew Poon <akpoon@gmail.com>
2019-07-08 00:08:55 -0700

    small corrections

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// const seed = require('../db/seed.js');
+app.use(express.json());
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
