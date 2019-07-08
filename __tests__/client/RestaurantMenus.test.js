df2bf55787d8f8eb54e232cc6a76acf50c603d12
Andrew Poon <akpoon@gmail.com>
2019-07-08 12:26:59 -0700

    test setup and other corrections to basic files structure

---
+++ __tests__/client/RestaurantMenus.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RestaurantMenus from '../../client/src/components/RestaurantMenus.jsx';
+
+describe('RestaurantMenus Enzyme Test Suite', () => {
+  it('component renders without crashing', () => {
+    shallow(<RestaurantMenus />);
+  });
+});
