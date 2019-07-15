75d2a5d4a55c31281c318936221b3005d96e5eb2
Andrew Poon <akpoon@gmail.com>
2019-07-15 11:37:11 -0700

    minor changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Restaurant = require('../db/Restaurant.js');
 
 const app = express();
-const port = 3000;
+const port = 3003;
 
 app.use(express.static('public'));
 
